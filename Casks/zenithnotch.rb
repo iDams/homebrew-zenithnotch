e4e4ee4797a20eb7d959d8744190568e78e6c246
Marco Astorga <marcoastorga.g@gmail.com>
2026-02-10 20:04:45 -0500

    Fixes sha256 checksum
    Updates the sha256 checksum for the ZenithNotch cask.

--- Casks/zenithnotch.rb
+++ Casks/zenithnotch.rb
@@ -1,7 +1,6 @@
 cask "zenithnotch" do
   version "0.1.42"
-  sha256 "de6f909ec7b9af3f4a00b12ace940d3ae424edc2b112828d5bc
-  ca911c4d00d4d" 
+  sha256 "de6f909ec7b9af3f4a00b12ace940d3ae424edc2b112828d5bcca911c4d00d4d"
 
   url "https://github.com/iDams/homebrew-zenithnotch/releases/download/v#{version}/ZenithNotch-v0.1-build42.dmg"
   name "ZenithNotch"
