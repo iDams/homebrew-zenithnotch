67e8baf61a3fd979c89152c737be7c6278cad371
Marco Astorga <39886325+iDams@users.noreply.github.com>
2026-02-10 19:29:28 -0500

    Update URLs and homepage for ZenithNotch cask

--- Casks/zenithnotch.rb
+++ Casks/zenithnotch.rb
@@ -2,11 +2,11 @@ cask "zenithnotch" do
     version "0.1.42"
     sha256 "b671fa3d1ec35f5a3cab3b1ee1f55019d3ce51aea65106fbf9f02fe1ea5b6ea7"
 
-    url "https://github.com/iDams/ZenithNotch/releases/download/v0.1.42/
+    url "https://github.com/iDams/homebrew-zenithnotch/releases/download/v0.1.42/
   ZenithNotch-v0.1-build42.dmg"
     name "ZenithNotch"
     desc "Utility notch experience for macOS"
-    homepage "https://github.com/iDams/ZenithNotch"
+    homepage "https://github.com/iDams/homebrew-zenithnotch"
 
     app "ZenithNotch.app"
   end
