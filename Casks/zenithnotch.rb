cd3d31503234a0e476b86a9bb9cee8cdf7945ba9
Marco Astorga <marcoastorga.g@gmail.com>
2026-02-12 17:44:53 -0500

    Updates to version 0.1.43
    Updates the cask to the latest version.
    This ensures users are running the most up-to-date
    version of the application with the newest features
    and bug fixes.

--- Casks/zenithnotch.rb
+++ Casks/zenithnotch.rb
@@ -1,8 +1,8 @@
 cask "zenithnotch" do
-  version "0.1.42"
-  sha256 "1e43f785a182ac73d71a643ee0f9f22624ee293d70ad806fdd6e11907dffce39"
+  version "0.1.43"
+  sha256 "10ac057e7f2559e322432d42d55608c573dea17b48067785a2ef4675ee7e16ee"
 
-  url "https://github.com/iDams/homebrew-zenithnotch/releases/download/v#{version}/ZenithNotch-v0.1-build42.dmg"
+  url "https://github.com/iDams/homebrew-zenithnotch/releases/download/v#{version}/ZenithNotch-v0.1-build43.dmg"
   name "ZenithNotch"
   desc "ZenithNotch application"
   homepage "https://github.com/iDams/homebrew-zenithnotch"
